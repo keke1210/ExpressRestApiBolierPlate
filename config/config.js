4eb18e955b3149ff9cf34170edc4cd0b4b8077c6
Skerdi Berberi <skerdi.berberi@kreatx.al>
2020-04-12 14:35:20 +0200

    testing gitignore

--- config/config.js
+++ config/config.js
@@ -6,5 +6,5 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 5000,
     URL: process.env.BASE_URL || 'http://localhost:5000',
-    MONGODB_URI: process.env.MONGODB_URI || 'YOUR_URI'
+    MONGODB_URI: process.env.MONGODB_URI || 'MY_URI'
 }
